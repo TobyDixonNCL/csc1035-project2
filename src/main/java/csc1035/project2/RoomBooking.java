3b53400d0ec68f6315c8b4a952f6a5801cb7ff69
Emabb <emabbers@gmail.com>
2021-03-16 20:19:12 +0000

    RoomBooking - added functionality for getting a list of rooms from the database, updated rest of code to include this.

--- src/main/java/csc1035/project2/RoomBooking.java
+++ src/main/java/csc1035/project2/RoomBooking.java
@@ -1,69 +1,85 @@
 package csc1035.project2;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RoomBooking {
 
-    //A way of producing a timetable for a room
-    //Make a list of every booking for a room and compile them together into a timetable
-    public static void ProduceRoomTimetable(BufferedReader consoleReader) {
-        //TODO: This is all commented out for now since it won't work until I can get the
-        //actual room list, which is being worked on by another member of the team.
+    public static List<RoomDetails> GetRoomList() {
+        Session session;
+        List<RoomDetails> roomList = new ArrayList<>();
+        session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
 
-        //TODO: have it get the actual room list, may be able to add it in as an argument instead
-        //List<Room> roomList;
+        try {
+            //create list of rooms
+            List dbRead = session.createQuery("FROM Rooms").list();
 
-        //asks the user for a room to make the timetable for
-        //System.out.printf("%nChoose a room to get the timetable for:");
-        //chosenRoom = ChooseRoom(consoleReader);
+            //loop and add each record to an arraylist
+            //parse to RoomDetails object
+            roomList.addAll(dbRead);
 
-        //Gets the list of bookings from the selected room, then prints the details of each individual booking.
-        //List BookingList = chosenRoom.getBookingList
 
-        //for (Booking x: BookingList) {
-        //System.out.println("Details of X");
-
+        } catch (HibernateException e) {
+            if (session != null) session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
         }
 
+        return roomList;
+    }
+
+    //A way of producing a timetable for a room
+    //Make a list of every booking for a room and compile them together into a timetable
+    public static void ProduceRoomTimetable(BufferedReader consoleReader) throws IOException {
+
+        List<RoomDetails> roomList = GetRoomList();
+
+        //asks the user for a room to make the timetable for
+        System.out.printf("%nChoose a room to get the timetable for:");
+        RoomDetails chosenRoom = ChooseRoom(consoleReader, roomList);
+
+
+
+        //for (Booking x: BookingList) {
+        System.out.println("Details of X");
+
+    }
 
 
     public static void UpdateRoomDetails(BufferedReader consoleReader) {
 
     }
 
-
-
-    //TODO: also add the roomlist as an argument
-    public static void ChooseRoom(BufferedReader consoleReader)
-    {
-        //Room chosenRoom = null;
-
-        //for each room in roomlist, print the room name and its position in the array
-        //NOTE: Arrays start at 0, this has the options start from 1 purely for aesthetic reasons, just make sure
-        //this is accounted for when getting the user choice or change it entirely.
-
-        //int loop = 0;
-        //for (Room x:roomList) {
-        //loop++;
-        //String roomName = x.getName();
-        //System.out.printf(loop + ": " + roomName);
-        //}
-
+    public static RoomDetails ChooseRoom(BufferedReader consoleReader, List<RoomDetails> roomList) throws IOException {
+        RoomDetails chosenRoom = null;
+        //for each room in room list, print the room name and its position in the array
+        int loop = 0;
+        for (RoomDetails x : roomList) {
+            System.out.println(loop + ": " + x.getRoomID() + " " + x.getType());
+            loop++;
+        }
 
         //Gets an input from the user, then gets the room in the same array position
-        //int choice = 0;
-        //while (choice is not invalid) {
+        int choice;
+        boolean stopLoop = false;
+        while (!stopLoop) {
             //Parses user input as an integer. May need validation for catching non-integer inputs.
-            //choice = Integer.parseInt(consoleReader.readLine());
-            //choice = choice - 1; //because we incremented it by 1 when writing out room names
+            choice = Integer.parseInt(consoleReader.readLine());
 
-            //chosenRoom = roomList[choice]
-        //}
+            if ((choice >= 0) && (choice <= loop)) {
+                chosenRoom = roomList.get(choice);
+                stopLoop = true;
+            }
+        }
 
-        //}
-        //return chosenRoom;
+        return chosenRoom;
     }
 
 }
