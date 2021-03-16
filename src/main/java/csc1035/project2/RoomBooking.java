0ee8e5ad6ea8bbb2032b9c1d737ed5fe96811292
Emabb <emabbers@gmail.com>
2021-03-16 23:09:49 +0000

    RoomBooking - Added method to get list of room bookings Completed method to generate a room timetable Added method to update a room's details

--- src/main/java/csc1035/project2/RoomBooking.java
+++ src/main/java/csc1035/project2/RoomBooking.java
@@ -11,18 +11,17 @@ import java.util.List;
 public class RoomBooking {
 
     public static List<RoomDetails> GetRoomList() {
-        Session session;
+        Session session = HibernateUtil.getSessionFactory().openSession();
         List<RoomDetails> roomList = new ArrayList<>();
-        session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
 
         try {
+            session = HibernateUtil.getSessionFactory().openSession();
             //create list of rooms
-            List dbRead = session.createQuery("FROM Rooms").list();
+            List dbRooms = session.createQuery("FROM Rooms").list();
 
-            //loop and add each record to an arraylist
-            //parse to RoomDetails object
-            roomList.addAll(dbRead);
+            //parses database records as RoomDetails objects and adds them to the room list.
+            roomList.addAll(dbRooms);
 
 
         } catch (HibernateException e) {
@@ -35,25 +34,92 @@ public class RoomBooking {
         return roomList;
     }
 
+    public static List<Bookings> GetBookingList() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List<Bookings> bookingList = new ArrayList<>();
+
+        session.beginTransaction();
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            //create list of bookings
+            List dbBookings = session.createQuery("FROM Bookings").list();
+
+            //parses database records as RoomDetails objects and adds them to the room list.
+            bookingList.addAll(dbBookings);
+
+
+        } catch (HibernateException e) {
+            if (session != null) session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return bookingList;
+    }
+
+
     //A way of producing a timetable for a room
     //Make a list of every booking for a room and compile them together into a timetable
     public static void ProduceRoomTimetable(BufferedReader consoleReader) throws IOException {
 
         List<RoomDetails> roomList = GetRoomList();
 
+        List<Bookings> bookingList = GetBookingList();
+
         //asks the user for a room to make the timetable for
         System.out.printf("%nChoose a room to get the timetable for:");
         RoomDetails chosenRoom = ChooseRoom(consoleReader, roomList);
+        //gets that room's ID
+        String chosenID = chosenRoom.getRoomID();
 
+        for (Bookings x : bookingList) {
+            if (x.getBookingID().equals(chosenID))
+            {
+                System.out.println("Booking " + x.getBookingID() + " on " + x.getTime() + " for " + x.getDuration());
+            }
+        }
 
-
-        //for (Booking x: BookingList) {
-        System.out.println("Details of X");
 
     }
 
 
-    public static void UpdateRoomDetails(BufferedReader consoleReader) {
+    public static void UpdateRoomDetails(BufferedReader consoleReader) throws IOException {
+
+        List<RoomDetails> roomList = GetRoomList();
+
+        System.out.println("Input Room ID");
+        String ID = consoleReader.readLine();
+        System.out.println("Input Room Max. Capacity");
+        int maxNormal =  Integer.parseInt(consoleReader.readLine());
+        System.out.println("Input Room Max. Capacity (Social Distancing)");
+        int maxCovid = Integer.parseInt(consoleReader.readLine());
+        System.out.println("Input Room Description");
+        String type = consoleReader.readLine();
+
+
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        try{
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+
+            //get row with same ID from Rooms table
+            RoomDetails updateRoom = (session.get(RoomDetails.class, ID));
+            //set the new values
+            updateRoom.setMaxCapacity(maxNormal);
+            updateRoom.setSocialDistancingCapacity(maxCovid);
+            updateRoom.setType(type);
+            System.out.println("Updated details.");
+
+        } catch (HibernateException e) {
+            if (session!=null) session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
 
     }
 
@@ -76,6 +142,9 @@ public class RoomBooking {
             if ((choice >= 0) && (choice <= loop)) {
                 chosenRoom = roomList.get(choice);
                 stopLoop = true;
+            }
+            else {
+                System.out.println("Invalid selection");
             }
         }
 
